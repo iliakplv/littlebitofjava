a202d24ab4527eae52e1e52b8b6e786d5332fc90
Ilia Kopylov <iliakplv@gmail.com>
2016-03-11 15:17:04 +1300

    xml parsing

--- src/com/iliakplv/java/AndroidMissingTranslationsFinder.java
+++ src/com/iliakplv/java/AndroidMissingTranslationsFinder.java
@@ -1,10 +1,14 @@
 package com.iliakplv.java;
 
-import java.io.BufferedReader;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -12,11 +16,12 @@ import java.util.Set;
  * Dummy Android missing translations finder.
  * Displays list of string names existing in default locale but missing in any other supported locale.
  *
- * Do not forget to change constants to your actual values before running!
+ * Do not forget to change RES_DIR constant to your actual value before running!
  */
 public final class AndroidMissingTranslationsFinder {
 
-    private static final String RES_DIR = "/Users/USER/workspace/PROJECT/app/src/main/res/";
+    private static final String RES_DIR = "/Users/USER/workspace/spike-android/app/src/main/res/";
+
     private static final String VALUES_DIR = "values";
     private static final String STRINGS_FILE = "strings.xml";
 
@@ -24,49 +29,105 @@ public final class AndroidMissingTranslationsFinder {
             "da", "de", "el", "en-rGB", "es", "fi", "fr", "it", "nl", "no", "pl", "pt", "ru", "sv", "tr"};
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        final Set<String> defaultStrings = getTranslations(null);
+        // set of strings in default locale
+        final Set<String> defaultStringsSet = getTranslations(null);
+
+        // map of missing string sets for each app locale (locale is a key)
+        final HashMap<String, Set<String>> missingStrings = new HashMap<>();
+
+        // set of string existing only in default locale but missing in all others
+        final Set<String> commonMissingStrings = new HashSet<>();
+
+
+        // find missing strings
+        for (String locale : LOCALES) {
+            final Set<String> localizedStringsSet = getTranslations(locale);
+            final Set<String> missingStringsSet = new HashSet<>();
+
+            for (String stringName : defaultStringsSet) {
+                if (!localizedStringsSet.contains(stringName)) {
+                    missingStringsSet.add(stringName);
+                }
+            }
+
+            missingStrings.put(locale, missingStringsSet);
+        }
+
+        // find common missing strings
+        for (String locale : LOCALES) {
+            final Set<String> localeMissingStrings = missingStrings.get(locale);
+            for (String string : localeMissingStrings) {
+                final boolean missingInAllLocales = isStringMissingInAllLocales(missingStrings, string);
+                if (missingInAllLocales) {
+                    commonMissingStrings.add(string);
+                }
+            }
+        }
+
+        // remove common missing string from locale specific sets
+        for (String locale : LOCALES) {
+            final Set<String> localeMissingStrings = missingStrings.get(locale);
+            for (String commonMissingString : commonMissingStrings) {
+                localeMissingStrings.remove(commonMissingString);
+            }
+        }
+
+        // Print results
+        System.out.println("\nStrings existing in default locale only (missing in all others):\n");
+        for (String commonMissingString : commonMissingStrings) {
+            System.out.println(commonMissingString);
+        }
+        System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
 
         for (String locale : LOCALES) {
-            System.out.println("Locale [" + locale + "] missing strings:");
-            final Set<String> localizedStrings = getTranslations(locale);
-            defaultStrings.stream().filter(string -> !localizedStrings.contains(string)).forEach(System.out::println);
-            System.out.println();
+            System.out.println(String.format("Strings missing in locale [%s]:\n", locale));
+            final Set<String> localeMissingStrings = missingStrings.get(locale);
+            for (String string : localeMissingStrings) {
+                System.out.println(string);
+            }
+            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
         }
 
     }
 
+    private static boolean isStringMissingInAllLocales(Map<String, Set<String>> localeMissingStringsMap, String stringToSearch) {
+        for (String locale : localeMissingStringsMap.keySet()) {
+            final Set<String> localeMissingStrings = localeMissingStringsMap.get(locale);
+            if (!localeMissingStrings.contains(stringToSearch)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-    private static Set<String> getTranslations(String locale) {
+
+    private static Set<String> getTranslations(String locale) throws Exception {
         final File stringsFile = isEmpty(locale) ?
                 new File(RES_DIR + VALUES_DIR + "/" + STRINGS_FILE) :
                 new File(RES_DIR + VALUES_DIR + "-" + locale + "/" + STRINGS_FILE);
 
-        final HashSet<String> stringsSet = new HashSet<>();
 
-        try {
-            try (BufferedReader br = new BufferedReader(new FileReader(stringsFile))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    final String namePrefix = "name=\"";
-                    final String nameSuffix = "\">";
-                    final int nameIndex = line.indexOf(namePrefix);
-                    final int nameEndIndex = line.indexOf(nameSuffix);
+        final NodeList stringList = getXmlNodeList(stringsFile, "string");
+        final HashSet<String> stringSet = new HashSet<>();
 
-                    if (nameIndex > 0 && nameEndIndex > 0) {
-                        final int nameStartIndex = nameIndex + namePrefix.length();
-                        stringsSet.add(line.substring(nameStartIndex, nameEndIndex));
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < stringList.getLength(); i++) {
+            String stringName = stringList.item(i).getAttributes().getNamedItem("name").getNodeValue();
+            stringSet.add(stringName);
         }
 
-        return stringsSet;
+        return stringSet;
     }
 
+    private static NodeList getXmlNodeList(File xmlFile, String nodeTagName) throws Exception {
+        DocumentBuilderFactory documentBuilderFactory
+                = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(xmlFile);
+        document.getDocumentElement().normalize();
+        return document.getElementsByTagName(nodeTagName);
+    }
 
     private static boolean isEmpty(String string) {
         return string == null || string.length() == 0;
